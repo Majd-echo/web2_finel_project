a3ddbc4acd376100eae55ed5aca3e04ec77d693e
Majd-echo <majd.2002.2020@gmail.com>
2022-06-08 23:12:50 +0300

    first module

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Category;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' =>$this->faker->sentence(8),
+            'contener' =>$this->faker->text(),
+            'user_id' =>$this->faker->randomElement(User::all())->id,
+            'categoty_id' =>$this->faker->randomElement(Category::all())->id,
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->create();
+        Category::factory(4)->create();
+        Post::factory(15)->create();
+        Tag::factory(8)->create();
+    }
+}
